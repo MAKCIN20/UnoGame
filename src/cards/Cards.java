cbaff213a9304feefe0671e9564f172fa8e9c7f0
MAKCIN20 <77026496+MAKCIN20@users.noreply.github.com>
2024-05-01 13:19:21 +0300

    Cards fixed login page added user is fixed Main is still for testing

--- src/cards/Cards.java
+++ src/cards/Cards.java
@@ -1,81 +1,64 @@
 package cards;
 
-import java.util.Random;
+import java.util.*;
 
 public class Cards {
     public int score;
+    public ArrayList<Cards> deck;
 
-    public NumberCard[] intiliazeNumberCard() {
-        String[] colorOptions= {"red", "blue", "green", "yellow"};
-        NumberCard[] numberCards = new NumberCard[76];
-        int index=0;
-        for (String colors: colorOptions) {
-            for (int i = 0; i < 10; i++) {
-                if(i == 0) {
-                    numberCards[index] = new NumberCard(i, colors);
-                    index++;
-                }
-                else {
-                    numberCards[index] = new NumberCard(i, colors);
-                    index++;
-                    numberCards[index] = new NumberCard(i, colors);
-                    index++;
-                }
+    public Cards() {
+        this.score = 0;
+        this.deck = new ArrayList<>();
+    }
+
+    public void initializer() {
+        List<NumberCard> numberCards = initializeNumberCard();
+        List<ActionCards> actionCards = initializeActionCards();
+        List<WildCard> wildCards = initializeWildCard();
+
+        deck.addAll(numberCards);
+        deck.addAll(actionCards);
+        deck.addAll(wildCards);
+        shuffleCards();
+    }
+
+    public List<NumberCard> initializeNumberCard() {
+        String[] colorOptions = {"red", "blue", "green", "yellow"};
+        List<NumberCard> numberCards = new ArrayList<>();
+        for (String color : colorOptions) {
+            numberCards.add(new NumberCard(0, color));
+            for (int i = 1; i < 10; i++) {
+                numberCards.add(new NumberCard(i, color));
+                numberCards.add(new NumberCard(i, color));
             }
         }
         return numberCards;
     }
-    public ActionCards[] initiliazeActionCards() {
-        String[] colorOptions= {"red", "blue", "green", "yellow"};
-        String[] actionOptions= {"skip", "reverse", "draw2"};
-        ActionCards[] actionCards = new ActionCards[24];
-        int index=0;
-        for (String options: actionOptions) {
-            for (String colors : colorOptions) {
-                for (int i = 0; i < 2; i++) {
-                    actionCards[index] = new ActionCards(options, colors);
-                    index++;
-                }
+
+    public List<ActionCards> initializeActionCards() {
+        String[] colorOptions = {"red", "blue", "green", "yellow"};
+        String[] actionOptions = {"skip", "reverse", "draw2"};
+        List<ActionCards> actionCards = new ArrayList<>();
+        for (String action : actionOptions) {
+            for (String color : colorOptions) {
+                actionCards.add(new ActionCards(action, color));
+                actionCards.add(new ActionCards(action, color));
             }
         }
         return actionCards;
     }
 
-    public WildCard[] initializeWildCard() {
-        WildCard[] wildCards = new WildCard[8];
+    public List<WildCard> initializeWildCard() {
+        List<WildCard> wildCards = new ArrayList<>();
         String[] wildOptions = {"wild", "draw4"};
-        int index = 0;
-        for (String options: wildOptions) {
+        for (String option : wildOptions) {
             for (int i = 0; i < 4; i++) {
-                wildCards[index] = new WildCard(options);
-                index++;
+                wildCards.add(new WildCard(option));
             }
         }
         return wildCards;
     }
-    public void shuffleCards(NumberCard[] numberCards, ActionCards[] actionCards, WildCard[] wildCards){
-        Random random = new Random();
-        for (int i = 0; i < 1000; i++) {
-            int randomIndex1 = random.nextInt(76);
-            int randomIndex2 = random.nextInt(76);
-            NumberCard temp = numberCards[randomIndex1];
-            numberCards[randomIndex1] = numberCards[randomIndex2];
-            numberCards[randomIndex2] = temp;
-        }
-        for (int i = 0; i < 1000; i++) {
-            int randomIndex1 = random.nextInt(24);
-            int randomIndex2 = random.nextInt(24);
-            ActionCards temp = actionCards[randomIndex1];
-            actionCards[randomIndex1] = actionCards[randomIndex2];
-            actionCards[randomIndex2] = temp;
-        }
-        for (int i = 0; i < 1000; i++) {
-            int randomIndex1 = random.nextInt(8);
-            int randomIndex2 = random.nextInt(8);
-            WildCard temp = wildCards[randomIndex1];
-            wildCards[randomIndex1] = wildCards[randomIndex2];
-            wildCards[randomIndex2] = temp;
-        }
+    public void shuffleCards() {
+        Collections.shuffle(deck);
     }
-
 }
--- src/test/Main.java
+++ src/test/Main.java
@@ -1,18 +1,31 @@
-package test;
-import LoginPage.User;
-import cards.ActionCards;
-import cards.Cards;
-import cards.NumberCard;
-import cards.WildCard;
+/************** Pledge of Honor ******************************************
+ I hereby certify that I have completed this programming project on my own
+ without any help from anyone else. The effort in the project thus belongs
+ completely to me. I did not search for a solution, or I did not consult any
+ program written by others or did not copy any program from other sources. I
+ read and followed the guidelines provided in the project description.
+ READ AND SIGN BY WRITING YOUR NAME SURNAME AND STUDENT ID
+ SIGNATURE: <Mehmet Emin Akçin, 76517>
+ *************************************************************************/
 
+
+package test;
+
+import GUI.WelcomePage;
+
+import cards.Cards;
 public class Main {
     public static void main(String[] args) {
-        Cards card = new Cards();
-        NumberCard[] deck=card.intiliazeNumberCard();
-        ActionCards[] actionDeck=card.initiliazeActionCards();
-        WildCard[] wildDeck=card.initializeWildCard();
-        User x=new User(false);
-        System.out.println(x);
+        String username;
+        Cards cards=new Cards();
+        cards.initializer();
+        System.out.println(cards.deck);
+        WelcomePage x=new WelcomePage();
+
+        username=x.username;
+        System.out.println(username);
+
+
 
 
     }
